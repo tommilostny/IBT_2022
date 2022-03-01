050079e24e9e264dac04ca6087f1f25d19da0dc3
tommilostny <tommilostny@live.com>
2022-03-01 18:11:27 +0100

    Updated simulator to generate better base room temperature data. (TODO: add weather effect)

--- src/SmartHeater.ML.Simulator/Program.cs
+++ src/SmartHeater.ML.Simulator/Program.cs
@@ -1,26 +1,43 @@
-﻿var date = new DateTime(2020, 1, 1);
+﻿//Heater parameters with preset values.
+var powerState = false;
+var roomTemp = 18.5;
+bool switchFlag;
+
+//Other environment and helper variables.
+var date = new DateTime(2020, 1, 1);
+var random = new Random();
 using var sw = new StreamWriter("training.csv");
 
-await sw.WriteLineAsync("date_time;temperature;turned_on");
+//Write csv file headers.
+await sw.WriteLineAsync("date_time;temp_diff;turned_on");
 
-for (uint currentTime = 0; date.Year == 2020; currentTime++, date = date.AddMinutes(1))
+//Start the main loop for year 2020.
+while (date.Year == 2020)
 {
-    var roomTemp = RoomTemperature(currentTime);
-    var shouldBeOn = HeaterOn(roomTemp);
+    //Get reference room temperature and next heater status.
+    var refTemp = ReferenceTemperature();
+    var shouldBeOn = HeaterOn(refTemp);
 
-    await sw.WriteLineAsync($"{date};{roomTemp};{shouldBeOn}");
-}
+    //Write heater status to the csv.
+    await sw.WriteLineAsync($"{date};{roomTemp - refTemp};{shouldBeOn}");
 
-static double RoomTemperature(double time)
-{
-    if (time == 0)
+    //Update heater power status and room temperature.
+    var oldPowerState = powerState;
+    powerState = Convert.ToBoolean(shouldBeOn);
+    if (!(switchFlag = oldPowerState != powerState))
     {
-        time += 0.0000001;
+        roomTemp += (powerState ? random.Next(1, 3) : random.Next(-1, 0)) / 10.0;
+        roomTemp += (powerState ? random.Next(-1, 4) : random.Next(-6, -1)) / 100.0;
     }
-    return -8 * Math.Sin(time / 2) / time;
+
+    //Advance time.
+    date = date.AddMinutes(1);
 }
 
-static int HeaterOn(double roomTemperature)
-{
-    return roomTemperature < 0 ? 1 : 0;
-}
+int HeaterOn(double refTemp) => powerState
+    ? (roomTemp <= refTemp + 0.16 ? 1 : 0)
+    : (roomTemp <= refTemp - 0.23 ? 1 : 0);
+
+double ReferenceTemperature() => (date.Hour >= 23 && date.Minute >= 30) || date.Hour <= 5
+    ? 21.5 //night
+    : 23.1;//day
