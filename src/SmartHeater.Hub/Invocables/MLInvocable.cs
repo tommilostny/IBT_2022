53c39939973551b508255bbc6fe983483fd06184
tommilostny <tommilostny@live.com>
2022-03-24 22:01:15 +0100

    Updated ML invocable logic to send commands to heater. Maybe think of something more complicated?

--- src/SmartHeater.Hub/Invocables/MLInvocable.cs
+++ src/SmartHeater.Hub/Invocables/MLInvocable.cs
@@ -23,6 +23,7 @@ public class MLInvocable : IInvocable
 
     private async Task ForecastHeaterAction(HeaterListModel heater)
     {
+        //Get data from heater for model input.
         var heaterService = _heatersProvider.GetHeaterService(heater);
         if (heaterService is null)
         {
@@ -38,14 +39,48 @@ public class MLInvocable : IInvocable
             TemperatureDiff = (float)status.Temperature - heater.ReferenceTemperature
         };
 
+        //Perform forecasting
+        var forecast = ForecastAndPrint(heater, input);
+
+        //Control heater based on forecasted trend.
+        //Upward trend (>0): turn off
+        //Downward trend (<=0): turn on
+        var trend = ForecastingTrend(forecast);
+        if (trend > 0 && status?.IsTurnedOn == true)
+        {
+            Console.WriteLine("sending off command");
+            await heaterService.TurnOffAsync();
+        }
+        else if (trend < 0 && status?.IsTurnedOn == false)
+        {
+            Console.WriteLine("sending on command");
+            await heaterService.TurnOnAsync();
+        }
+    }
+
+    private static ModelOutput ForecastAndPrint(HeaterListModel heater, ModelInput input)
+    {
         Console.WriteLine($"starting forecast for diff: {input.TemperatureDiff}");
+
         var forecast = SmartHeaterModel.Forecast(heater.IpAddress, input);
-        Console.WriteLine("end of forecast");
 
         Console.WriteLine("----------------------------------");
         foreach (var item in forecast.TemperatureDiff)
         {
             Console.WriteLine(item);
         }
+        return forecast;
+    }
+
+    private static float ForecastingTrend(ModelOutput forecast)
+    {
+        var diffs = new List<float>();
+        for (int i = 0; i < forecast.TemperatureDiff.Length - 1; i++)
+        {
+            diffs.Add(forecast.TemperatureDiff[i + 1] - forecast.TemperatureDiff[i]);
+        }
+        var trend = diffs.Average();
+        Console.WriteLine($"trend: {trend}");
+        return trend;
     }
 }
