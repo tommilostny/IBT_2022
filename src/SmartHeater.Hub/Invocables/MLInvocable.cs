8b6f7574f3ac11815aa9b65df2a803bd6537ecd6
tommilostny <tommilostny@live.com>
2022-04-02 12:07:02 +0200

    Updated ML invocable cosmeticly.

--- src/SmartHeater.Hub/Invocables/MLInvocable.cs
+++ src/SmartHeater.Hub/Invocables/MLInvocable.cs
@@ -40,67 +40,73 @@ public class MLInvocable : IInvocable
         };
 
         //Perform forecasting
-        var forecast = GetForecast(heater, input);
+        #if DEBUG
+            var forecast = GetAndPrintForecast(heater, input);
+        #else
+            var forecast = SmartHeaterModel.Forecast(heater.IpAddress, input);
+        #endif
 
         //Check for over or under-heating and get forecasted trend (function direction).
-        //These variables are used as follows:
+        //These metrics variables are used as follows:
         //  Overheating => turn off,
         //  Underheating => turn on,
         //  else if upward trend (>0) => turn on,
         //  else if downward trend (<0) => turn off.
-        var overheating = forecast.TemperatureDiff.All(x => x > 0) && forecast.TemperatureDiff.Average() > 0.25;
-        var underheating = forecast.TemperatureDiff.All(x => x < 0) && forecast.TemperatureDiff.Average() < -0.25;
-        var trend = ForecastingTrend(forecast);
+        (var overheating, var underheating, var trend) = GetDecisionMetrics(forecast);
 
-#if DEBUG
-        Console.WriteLine($"Overheating: {overheating}");
-        Console.WriteLine($"Underheating: {underheating}");
-#endif
         if (overheating || (trend < 0 && !underheating))
         {
-#if DEBUG
-            Console.WriteLine("sending off command");
-#endif
             if (status.IsTurnedOn == true)
                 await heaterService.TurnOffAsync();
+
+            #if DEBUG
+                Console.WriteLine("sending off command");
+            #endif
         }
         else if (underheating || (trend > 0 && !overheating))
         {
-#if DEBUG
-            Console.WriteLine("sending on command");
-#endif
             if (status.IsTurnedOn == false)
                 await heaterService.TurnOnAsync();
+
+            #if DEBUG
+                Console.WriteLine("sending on command");
+            #endif
         }
     }
 
-    private static ModelOutput GetForecast(HeaterListModel heater, ModelInput input)
+#if DEBUG
+    private static ModelOutput GetAndPrintForecast(HeaterListModel heater, ModelInput input)
     {
-#if DEBUG
         Console.WriteLine($"starting forecast for diff: {input.TemperatureDiff}");
-#endif
+
         var forecast = SmartHeaterModel.Forecast(heater.IpAddress, input);
-#if DEBUG
-        Console.WriteLine("----------------------------------");
+        
         foreach (var item in forecast.TemperatureDiff)
         {
             Console.WriteLine(item);
         }
-#endif
         return forecast;
     }
+#endif
 
-    private static float ForecastingTrend(ModelOutput forecast)
+    private static (bool, bool, float) GetDecisionMetrics(ModelOutput forecast)
     {
+        var overheating = forecast.TemperatureDiff.All(x => x > 0) && forecast.TemperatureDiff.Average() > 0.25;
+        var underheating = forecast.TemperatureDiff.All(x => x < 0) && forecast.TemperatureDiff.Average() < -0.25;
+
         var diffs = new List<float>();
         for (int i = 0; i < forecast.TemperatureDiff.Length - 1; i++)
         {
             diffs.Add(forecast.TemperatureDiff[i + 1] - forecast.TemperatureDiff[i]);
         }
         var trend = diffs.Average();
-#if DEBUG
-        Console.WriteLine($"trend: {trend}");
-#endif
-        return trend;
+
+        #if DEBUG
+            Console.WriteLine($"Overheating: {overheating}");
+            Console.WriteLine($"Underheating: {underheating}");
+            Console.WriteLine($"Trend: {trend}");
+        #endif
+        
+        return (overheating, underheating, trend);
     }
 }
